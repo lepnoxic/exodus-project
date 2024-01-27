b0d71539b9375f66448867e0e53389fd3ab9d764
Lepnoxic <akashdevulapalli@gmail.com>
2024-01-28 03:22:18 +0530

    delivery bro

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router';
 
+const navigate = useNavigate;
 
 const Delivery = () => {
     useEffect(() => {
         localStorage.removeItem('cartItems')
     }, []);
-  return (
-    <div>Thank you for your service</div>
+
+    return (
+    <div>Thank you for buying
+        <div>
+            <button className='btn' onClick={navigate('/items')}>
+                Return to Items
+            </button>
+            <button className='btn btn-error' onClick={navigate('/home')}>
+                Log Out
+            </button>
+        </div>
+    </div>
   )
 }
 
