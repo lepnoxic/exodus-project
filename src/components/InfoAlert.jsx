6f30e67505f7dd4300bc60b48999e67d65b1b13e
Lepnoxic <akashdevulapalli@gmail.com>
2024-01-28 18:04:16 +0530

    info bro

--- src/components/InfoAlert.jsx
+++ src/components/InfoAlert.jsx
@@ -15,7 +15,7 @@ function InfoAlert({ message }) {
     <div className={`fixed top-0 left-[20vw] w-[60vw] transition-transform duration-500 ${isVisible ? 'translate-y-4' : '-translate-y-full'}`}>
       <div role="alert" className="alert">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-        <span>12 unread messages. Tap to see.</span>
+        <span>{message}</span>
     </div>
     </div>
   );
